893896ca0e2a7f6f62aeb2e04cc9b7a3d05034df
Nsarkar-XLR8 <knayemlol24@gmail.com>
2024-01-02 07:52:44 +0600

    initial version

---
+++ Practise.py
@@ -0,0 +1,78 @@
+import math
+
+
+#
+# print("Hello, World")
+# print('\U0001f600')
+# print('\U0001f601')
+# print('\U0001f602')
+# print('\U0001f603')
+# print('\U0001f604')
+# print('\U0001f605')
+# print('\U0001f606')
+# # print('\U+1f608')
+#
+#
+# '''
+# Hey , babyyyy
+#
+# wanna got out together?
+#
+# '''
+#
+# """
+# What is your name?
+# """
+#
+#
+#
+#
+#
+# print("Hey Baby, my name is \"Nayem Sarkar\" \n & wanna makeout tonight?")
+#
+# print("Hey",6,9, sep="~")
+
+
+# a = complex(8,2)
+# b = 5.999
+# c = 10
+# d = "Nayem"
+# print("The type of a is ", type(a))
+# print("The type of b is ", type(b))
+# print("The type of c is ", type(c))
+# print("The type of d is ", type(d))
+#
+#
+# print(a+b+c)
+#
+#
+#
+#
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
+#
+# print(int(a) + int(b))
+# print(int(a) - int(b))
+# print(int(a) * int(b))
+# print(int(a) / int(b))
+# print(int(a) % int(b))
+# print(int(a) // int(b))
+# print(int(a)**int(b))
+
+
+
+print("<<<---Designing A Basic Calculator--->>>")
+
+a = input("Enter The First Number: ")
+b = input("Enter The Second Number: ")
+
+print("The Result Of Summation: ",int(a)+int(b))
+print("The Result Of Substraction: ",int(a)-int(b))
+print("The Result Of Multiplication: ",int(a)*int(b))
+print("The Result Of Division: ",int(a)/int(b))
+print("The Result Of Power: ",int(a)**int(b))
+# print("The Result Of Exponential: ",int(a)***int(b))
+print("The Result Of Modulus: ",int(a)%int(b))
+print("The Result Of Floor Division: ",int(a)//int(b))
+print("The Result Of Ceil Division: ",math.ceil(int(a)/int(b)))
+
