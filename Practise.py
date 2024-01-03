ff94d4215ed973bac57f031f58da41215daa4de1
Nsarkar-XLR8 <knayemlol24@gmail.com>
2024-01-03 11:51:07 +0600

    initial version

--- Practise.py
+++ Practise.py
@@ -1,78 +1,20 @@
-import math
+
+# Making Greeting Text Simulator
 
 
-#
-# print("Hello, World")
-# print('\U0001f600')
-# print('\U0001f601')
-# print('\U0001f602')
-# print('\U0001f603')
-# print('\U0001f604')
-# print('\U0001f605')
-# print('\U0001f606')
-# # print('\U+1f608')
-#
-#
-# '''
-# Hey , babyyyy
-#
-# wanna got out together?
-#
-# '''
-#
-# """
-# What is your name?
-# """
-#
-#
-#
-#
-#
-# print("Hey Baby, my name is \"Nayem Sarkar\" \n & wanna makeout tonight?")
-#
-# print("Hey",6,9, sep="~")
+import time
+Name = input("Tell Me Your Name: ")
+hours = int(time.strftime("%H"))
 
-
-# a = complex(8,2)
-# b = 5.999
-# c = 10
-# d = "Nayem"
-# print("The type of a is ", type(a))
-# print("The type of b is ", type(b))
-# print("The type of c is ", type(c))
-# print("The type of d is ", type(d))
-#
-#
-# print(a+b+c)
-#
-#
-#
-#
-# a = input("Enter a number: ")
-# b = input("Enter another number: ")
-#
-# print(int(a) + int(b))
-# print(int(a) - int(b))
-# print(int(a) * int(b))
-# print(int(a) / int(b))
-# print(int(a) % int(b))
-# print(int(a) // int(b))
-# print(int(a)**int(b))
+if(hours >=4 and hours <=12):
+    print("Good Morning,",Name)
+elif(hours >=12 and hours <= 16):
+    print("Good Afternoon,",Name)
+elif(hours >=16 and hours<= 19):
+    print("Good Evening,",Name)
+else:
+    print("Good Night,",Name)
 
 
 
-print("<<<---Designing A Basic Calculator--->>>")
-
-a = input("Enter The First Number: ")
-b = input("Enter The Second Number: ")
-
-print("The Result Of Summation: ",int(a)+int(b))
-print("The Result Of Substraction: ",int(a)-int(b))
-print("The Result Of Multiplication: ",int(a)*int(b))
-print("The Result Of Division: ",int(a)/int(b))
-print("The Result Of Power: ",int(a)**int(b))
-# print("The Result Of Exponential: ",int(a)***int(b))
-print("The Result Of Modulus: ",int(a)%int(b))
-print("The Result Of Floor Division: ",int(a)//int(b))
-print("The Result Of Ceil Division: ",math.ceil(int(a)/int(b)))
 
